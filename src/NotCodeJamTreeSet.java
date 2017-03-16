c4966d330d091281881335a05b165fd987689848
johirbuet <johirbuet@gmail.com>
2017-03-15 21:01:04 -0500

    TreeSet Data Structure

---
+++ src/NotCodeJamTreeSet.java
@@ -0,0 +1,19 @@
+import java.util.TreeSet;
+
+public class NotCodeJamTreeSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet<String> tset =new TreeSet<>();
+		tset.add("Johir");
+		tset.add("Alimuddin");
+		tset.add("Arif");
+		tset.add("Kabir");
+		tset.add("Sifat");
+		// Stored in sorted order
+		for (String string : tset) {
+			System.out.println(string);
+		}
+	}
+
+}
